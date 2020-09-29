9e9a07ec8b71b2f0a0a4c805bfcb6d21a89cb013
Helga312 <may.traum@gmail.com>
2020-09-29 15:47:39 +0300

    feat: add solutions for Count cats and Carbon dating

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+    let numberCats = 0
+  
+    for (let i = 0; i < backyard.length; i++) { // перебор вложенных массивов
+      for (let j = 0; j < backyard[i].length; j++) { // перебор элементов вложенных массивов
+        if (backyard[i][j] === '^^') {
+          numberCats ++
+        } else continue
+      }
+    }
+    return numberCats
 };
