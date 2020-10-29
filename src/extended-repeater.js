82f8f381b4cc86aee4bf3986f3f34827a5ff9eaa
Helga312 <may.traum@gmail.com>
2020-10-29 17:05:12 +0300

    add solutions for extended repeater and hanoi tower

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,58 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let string = "";
+  let addition = "";
+
+  str === null ? (str = "null") : (str = str.toString());
+  options.separator === null ? (options.separator = "null") : options.separator;
+  options.addition === null ? (options.addition = "null") : options.addition;
+  options.additionSeparator === null
+    ? (options.additionSeparator = "null")
+    : options.additionSeparator;
+
+  typeof options.separator === "boolean"
+    ? (options.separator = options.separator.toString())
+    : options.separator;
+  typeof options.addition === "boolean"
+    ? (options.addition = options.addition.toString())
+    : options.addition;
+  typeof options.additionSeparator === "boolean"
+    ? (options.additionSeparator = options.additionSeparator.toString())
+    : options.additionSeparator;
+
+  options.repeatTimes ? options.repeatTimes : (options.repeatTimes = 1);
+  options.separator
+    ? (options.separator = options.separator.toString())
+    : (options.separator = "");
+  options.addition
+    ? (options.addition = options.addition.toString())
+    : (options.addition = "");
+  options.additionRepeatTimes
+    ? options.additionRepeatTimes
+    : (options.additionRepeatTimes = 1);
+  options.additionSeparator
+    ? (options.additionSeparator = options.additionSeparator.toString())
+    : (options.additionSeparator = "");
+
+  for (let add = 1; add <= options.additionRepeatTimes; add++) {
+    if (options.additionSeparator && add !== options.additionRepeatTimes) {
+      addition = `${addition}${options.addition}${options.additionSeparator}`;
+    } else if (add === options.additionRepeatTimes) {
+      addition = `${addition}${options.addition}`;
+    } else {
+      addition = `${addition}${options.addition}|`;
+    }
+  }
+
+  for (let countStr = 1; countStr <= options.repeatTimes; countStr++) {
+    if (countStr !== options.repeatTimes && options.separator) {
+      string = `${string}${str}${addition}${options.separator}`;
+    } else if (countStr === options.repeatTimes) {
+      string = `${string}${str}${addition}`;
+    } else {
+      string = `${string}${str}${addition}+`;
+    }
+  }
+  return string;
 };
-  
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let result = {};
+  result.turns = 2 ** disksNumber - 1;
+
+  let speed = turnsSpeed / 3600;
+  result.seconds = Math.floor(result.turns / speed)
+
+  return result;
+
 };
